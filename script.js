cc019d692cff719691bba7a900fe137d913f2ac0
Doruk Yılmaz <114815196+DorukYILMAZ@users.noreply.github.com>
2022-10-02 20:48:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ 
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Web Developer", "Designer", "Freelancer","Anxlize"],
+        typeSpeed: 80,
+        backSpeed: 48,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
